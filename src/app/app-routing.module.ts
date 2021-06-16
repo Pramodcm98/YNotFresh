937b2be45ba4f04b01b8ec377666c9b40abf99fe
Pramod <pramodcm98@gmial.com>
2021-06-16 17:01:22 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './profile/login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './profile/signup/signup.component';
+import { FishComponent } from './purchase/products/fish/fish.component';
+import { GroceryComponent } from './purchase/products/grocery/grocery.component';
+import { MeatComponent } from './purchase/products/meat/meat.component';
+import { PurchaseComponent } from './purchase/purchase.component';
+
+
+const routes: Routes = [
+  {path:'', pathMatch:"full",redirectTo:"/home"},//base rooting or default rooting or route redirecting
+  { path: "home", component: HomeComponent },
+
+  {path:"profile",component:ProfileComponent,
+   children:[
+     {path:'',component:LoginComponent},
+     {path:"login",component:LoginComponent},
+     {path:"signup",component:SignupComponent}
+    ]},
+
+  {path: "purchase", component: PurchaseComponent,
+    children:
+      [
+
+        {path:'',component:GroceryComponent},
+        { path: "grocery", component: GroceryComponent },
+        { path: "meat", component: MeatComponent },
+        { path: "fish", component: FishComponent }
+      ]
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { PurchaseComponent } from './purchase/purchase.component';
+import { FilterComponent } from './purchase/filter/filter.component';
+import { ProductsComponent } from './purchase/products/products.component';
+import { CartComponent } from './purchase/cart/cart.component';
+import { GroceryComponent } from './purchase/products/grocery/grocery.component';
+import { MeatComponent } from './purchase/products/meat/meat.component';
+import { FishComponent } from './purchase/products/fish/fish.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './profile/signup/signup.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PurchaseComponent,
+    FilterComponent,
+    ProductsComponent,
+    CartComponent,
+    GroceryComponent,
+    MeatComponent,
+    FishComponent,
+    ProfileComponent,
+    SignupComponent,
+   
   ],
   imports: [
     BrowserModule,
